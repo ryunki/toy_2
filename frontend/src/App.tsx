6ad31384ccf16ee5802112c22140dc8764eaf98d
Ryunki Shin <ryunkishin@gmail.com>
2022-12-18 21:37:25 +0900

    carousel added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,39 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import Menu from './pages/Menu';
+import Contact from './pages/Contact';
 import About from './pages/About';
 
 import './App.css';
+
 import { Container } from 'react-bootstrap';
+import CarouselFade from './components/CarouselFade';
+import Footer from './components/Footer';
 
 function App() {
-  return <>
-    <Navbar/>
-  <Container className="mb-4">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/menu" element={<Menu/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/*" element={<Navigate to="/"/>}/>
-      </Routes>
-    </Router>
-  </Container>
-  </>
+  return (
+    <>
+      <CarouselFade/>
+      <Navbar/>
+      <Container className="mb-4" >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
+      <Footer/>
+    </>
+  
+  );
 }
 
 export default App;
---
+++ frontend/src/components/CarouselFade.tsx
@@ -0,0 +1,31 @@
+import {Carousel} from 'react-bootstrap';
+
+import './Carousel.css'
+
+function CarouselFade() {
+const imgUrl = [
+  "/background.jpg",
+  "/background2.jpg",
+  
+]
+
+  return (
+    <Carousel fade interval={2000}>
+      {imgUrl.map((item, idx)=>(
+        <Carousel.Item key={idx}>
+          <img
+            className="d-block w-100 bg-picture"
+            src={item}
+            alt={idx + " slide"}
+          />
+          <Carousel.Caption>
+            <h3>{idx} slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
+
+export default CarouselFade;
