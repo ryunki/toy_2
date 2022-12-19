0a2964e03246191737ff09defb7775101ab9b0c6
Ryunki Shin <ryunkishin@gmail.com>
2022-12-19 14:08:44 +0900

    minor fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -14,8 +14,7 @@ const Navbar = () => {
     <NavbarBs className="bg-light navbar shadow-sm mb-3">
         <Row className="ms-4">
           <Col className="col-logo" onClick={clickHandler}>
-            {/* <img src="/tokyoya_logo_white.png"/> */}
-              <img src="/tokyoya_logo.png" style={{width:"50px",height:"50px"}}/>
+              <img className="nav-logo" src="/tokyoya_logo.png"/>
               <h1 className="title-text">TOKYOYA</h1>
           </Col>
         </Row>
