4ed410c784191031ca9c2b146abf07e9558c2aca
Ryunki Shin <ryunkishin@gmail.com>
2022-12-20 21:19:03 +0900

    carousel parallex effect

--- frontend/src/components/CarouselFade.tsx
+++ frontend/src/components/CarouselFade.tsx
@@ -1,31 +1,51 @@
 
+import { useEffect, useState, useRef, useMemo, useCallback } from 'react';
 import {Carousel} from 'react-bootstrap';
 
 import './Carousel.css'
 
 function CarouselFade() {
+
+  // function handleScroll() {
+  //   if (ScrollY > 299) {
+  //     setScrollY(window.pageYOffset);
+  //     setScrollActive(true);
+  //   } else {
+  //     setScrollY(window.pageYOffset);
+  //     setScrollActive(false);
+  //   }
+  // }
+
+  const [y, setY] = useState(0);
+  const [ScrollActive, setScrollActive] = useState(false);
+  // const ref = useRef<HTMLDivElement>("")
+
+  function handleScroll() {
+    setY(-window.scrollY/2) //negative: makes image move slower upwards
+  }
+
+  useEffect(()=>{
+   window.addEventListener("scroll", handleScroll)
+    return ()=>{
+      window.removeEventListener("scroll", handleScroll)
+    }
+ },[])
 const imgUrl = [
   "/background.jpg",
   "/background2.jpg",
 ]
 
   return (
-
-    <Carousel fade interval={2000} >
+    <>
+    {/* <div className="d-block w-100 bg-picture"
+        style={{backgroundImage: `url(${imgUrl[0]})`, backgroundPositionY:y}}/> */}
+    <Carousel fade interval={2000}>
       {imgUrl.map((item, idx)=>(
         <Carousel.Item key={idx}>
           <div
             className="d-block w-100 bg-picture"
-            // src={item}
-            // alt={idx + " slide"}
-            style={{backgroundImage: `url(${item})`}}
+            style={{backgroundImage: `url(${item})`, backgroundPositionY:y}}
           />
-          {/* <img
-            className="d-block w-100 bg-picture"
-            src={item}
-            alt={idx + " slide"}
-            style={{backgroundImage: `url(${item})`}}
-          /> */}
           <Carousel.Caption>
             <h3>{idx} slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -33,7 +53,9 @@ const imgUrl = [
         </Carousel.Item>
       ))}
     </Carousel>
+    </>
   );
 }
 
 export default CarouselFade;
+
